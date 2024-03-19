c45e6f2beae2da9803a92abca00304fb406e643f
Vaibhav Srivastava <vabnix@hotmail.com>
2024-03-19 17:40:42 -0500

    building CRUD apis

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 const postgresClient = require('./db');
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 
 //routes
 
@@ -12,20 +14,57 @@ app.post('/todos', async (req, res) => {
     try {
         const { description } = req.body;
         const newTodo = await postgresClient.query("INSERT INTO TODO (description) values ($1)", [description]);
-        res.json(newTodo);
+
+        res.json(newTodo.rows[0]);
     } catch (error) {
-        console.log(error.message)
+        console.log(error.message);
     }
 })
 
 //get all todos
+app.get('/todos', async (req, res) => {
+    try {
+        const allTodos = await postgresClient.query("SELECT * from TODO");
+        res.json(allTodos.rows);
+    } catch (error) {
+        console.log(error.message);
+    }
+})
 
 //get a todo
+app.get('/todos/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const getSingleTodo = await postgresClient.query("SELECT * from TODO WHERE todo_id = $1", [id]);
+        res.json(getSingleTodo.rows[0]);
+    } catch (error) {
+        console.log(error.message);
+    }
+})
 
 //update a todo
+app.put('/todos/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { description } = req.body;
+        const updateTodo = await postgresClient.query("UPDATE TODO SET DESCRIPTION = $1 WHERE todo_id = $2", [description, id]);
+        res.json("Todo is updated");
+    } catch (error) {
+        console.log(error.message);
+    }
+});
 
 //delete a todo
+app.delete('/todos/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleteTodo = await postgresClient.query("DELETE FROM TODO WHERE todo_id = $1", [id]);
+        res.json("Todo is deleted");
+    } catch (error) {
+        console.log(error.message);
+    }
+});
 
 app.listen(3000, () => {
-    console.log("Server started at Port 3000")
+    console.log("Server started at Port 3000");
 });
